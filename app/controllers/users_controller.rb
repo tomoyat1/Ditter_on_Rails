8ece9c1f35193582a9d41b949b78c7be758af0d4
Hirokazu Kiyomaru <kiyomaru.tennis927@gmail.com>
2016-02-12 01:38:01 +0900

    Add signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params[:user].permit(:user_name, :screen_name, :email, :password, :comment)
+  end
+
 end
