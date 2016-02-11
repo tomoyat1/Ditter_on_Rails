a156253bb642d8c7741fcbbc765e5609f66ea8ae
Hirokazu Kiyomaru <kiyomaru.tennis927@gmail.com>
2016-02-12 01:37:32 +0900

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+	validates :user_name, presence: { message: "No user_name given"}
+	validates :screen_name, presence: { message: "No screen_name given"}
+	validates :email, presence: { message: "No email given"}
+	validates :password, presence: { message: "No password given"}
 end
