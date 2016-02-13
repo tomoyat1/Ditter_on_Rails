d845a9846ee9a950eec41f4f9a3805d715f1223b
Hirokazu Kiyomaru <kiyomaru.tennis927@gmail.com>
2016-02-13 16:53:00 +0900

    Add posting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,14 @@
 class PostsController < ApplicationController
+  def create
+    @user = User.find(params[:user_id])
+    @post = @user.posts.create(post_params)
+    redirect_to users_path
+  end
+
+  private
+
+  def post_params
+    params[:post].permit(:text)
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  validates :text, presence: true
+
 end
